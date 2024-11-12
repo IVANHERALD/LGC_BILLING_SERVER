cbaad163c7cad27509dde074b5b3212ffd294b11
Ivan herald <ivanheraldd@gmail.com>
2024-11-12 16:51:02 +0530

    casting and customer

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ import billRouter from './Routes/Bill.js';
 import dotenv from 'dotenv';
 import express from 'express';
 import mongoose from 'mongoose';
+import CustomerRouter from './Routes/Customer.js';
+import CastingRouter from './Routes/Casting.js';
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 app.use("/lgc",billRouter)
+app.use("/lgc",CustomerRouter)
+app.use("/lgc",CastingRouter)
 
 
 
