558f57ca805f2d4e0368b7bbf9fa8f7609a5e743
Ivan herald <ivanheraldd@gmail.com>
2025-02-10 16:13:24 +0530

    Feat:Update casting and consignee added

--- Controller/Casting.js
+++ Controller/Casting.js
@@ -41,7 +41,7 @@ export const fetchCasting = async (req, res, next) => {
           console.log(casting)
           return res.status(400).json({ message: 'Casting name is required for updating.' });
         }
-    
+        console.log(casting,updateCasting);
         const existingCasting= await CastingDetails.findOne({casting_name:casting});
         if (!existingCasting) {
           return res.status(404).json({ message: 'Casting not found.' });
