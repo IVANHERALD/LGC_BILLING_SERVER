7b5c1deff9410fab0c4ba0a974de1e990a9f56bc
Ivan herald <ivanheraldd@gmail.com>
2025-02-10 12:13:55 +0530

    Delete customer added

--- Routes/Customer.js
+++ Routes/Customer.js
@@ -1,8 +1,8 @@
-import { createCustomer, fetchCustomers } from '../Controller/Customer.js';
+import { createCustomer, deleteCustomer, fetchCustomers } from '../Controller/Customer.js';
 import express from 'express';
 
 const CustomerRouter=express();
 CustomerRouter.post("/savecustomer",createCustomer);
 CustomerRouter.get("/fetchcustomer",fetchCustomers);
-CustomerRouter.delete("/deletecustomer/:consignee_name",deleteCasting);
+CustomerRouter.delete("/deletecustomer/:consignee_name",deleteCustomer);
 export default CustomerRouter;
