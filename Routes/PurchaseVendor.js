c178537d77a47a531b93612419c992175c1bd813
Ivan herald <ivanheraldd@gmail.com>
2025-05-14 12:42:22 +0530

    Feat:Fetch Vendor developed

--- Routes/PurchaseVendor.js
+++ Routes/PurchaseVendor.js
@@ -1,6 +1,7 @@
 import express from 'express';
-import { createVendor } from '../Controller/PurchaseVendor.js';
+import { createVendor, fetchVendors } from '../Controller/PurchaseVendor.js';
 
 const VendorRouter=express();
 VendorRouter.post("/savevendor",createVendor);
+VendorRouter.get("/fetchvendor",fetchVendors)
 export default VendorRouter;
